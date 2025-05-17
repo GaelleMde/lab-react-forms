24d730af2796a1861fb3fb6ada04b047ce65dc77
Gaelle <gaelle.madelaine@gmail.com>
2025-05-17 16:08:31 +0200

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,125 @@ import StudentCard from "./components/StudentCard";
 import studentsData from "./assets/students.json";
 
 function App() {
-  const [students, setStudents] = useState(studentsData);
+  // Crear los STATES
 
+  const [students, setStudents] = useState(studentsData);
+  const [fullNameInputValue, setFullNameInputValue] = useState("");
+  const [profilImageInputValue, setProfilImageInputValue] = useState("");
+  const [phoneInputValue, setPhoneInputValue] = useState("");
+  const [emailInputValue, setEmailInputValue] = useState("");
+  const [programInputValue, setProgramInputValue] = useState("");
+  const [graduationYearInputValue, setgraduationYearInputValue] =
+    useState(2023);
+  const [hasGraduatedValue, sethasGraduatedValue] = useState(false);
+
+  // Linkear STATE con los inputs
+
+  const handlefullNameChange = (event) => {
+    setFullNameInputValue(event.target.value);
+  };
+
+  const handleprofilImageChange = (event) => {
+    setProfilImageInputValue(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setPhoneInputValue(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmailInputValue(event.target.value);
+  };
+
+  const handleProgramChange = (event) => {
+    setProgramInputValue(event.target.value);
+  };
+
+  const handleGraduationYearChange = (event) => {
+    setgraduationYearInputValue(event.target.value);
+  };
+
+  const handlehasGraduatedChange = (event) => {
+    sethasGraduatedValue(event.target.value);
+  };
+
+  // OnSubmit
+  const handleAddStudentChange = (event) => {
+    event.preventDefault();
+
+    const studentToAdd = {
+      fullName: fullNameInputValue,
+      profilImage: profilImageInputValue,
+      phone: phoneInputValue,
+      email: emailInputValue,
+      program: programInputValue,
+      graduationYear: graduationYearInputValue,
+      graduated: hasGraduatedValue,
+    };
+    setStudents([...students, studentToAdd]);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleAddStudentChange}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              onChange={handlefullNameChange}
+              value={fullNameInputValue}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              onChange={handleprofilImageChange}
+              value={profilImageInputValue}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              onChange={handlePhoneChange}
+              value={phoneInputValue}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={handleEmailChange}
+              value={emailInputValue}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              name="program"
+              onChange={handleProgramChange}
+              value={programInputValue}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,24 +143,28 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={handleGraduationYearChange}
+              value={graduationYearInputValue}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              onChange={handlehasGraduatedChange}
+              checked={hasGraduatedValue}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
